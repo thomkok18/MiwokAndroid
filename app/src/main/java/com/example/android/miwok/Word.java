0ec733894b75d2914fc04c54328b7adcf840e00d
Thom Kok <tkok@roc-dev.com>
2017-02-22 22:14:11 +0100

    update

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,19 @@
+package com.example.android.miwok;
+
+public class Word {
+    private String mMiwokTranslation;
+    private String mDefaultTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mMiwokTranslation = miwokTranslation;
+        mDefaultTranslation = defaultTranslation;
+    }
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
